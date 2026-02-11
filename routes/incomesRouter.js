2f0019a578f48200c5211c8f65ef20de31688d5b
EmmaRask <emmarask@yahoo.com>
2026-02-11 14:31:04 +0100

    deleted emptey spaces

--- routes/incomesRouter.js
+++ routes/incomesRouter.js
@@ -34,35 +34,3 @@ router.post('/', (req, res, next) => {
 });
 
 export default router;
-
-
-
-
-
-
-
-// import express from 'express';
-// import { incomes } from '../data/store.js';
-
-// const router = express.Router();
-
-
-// router.post('/', (req, res) => {
-//     // POST data
-//     const incomeAmount = Number(req.body.income);
-//     if(!isNaN(incomeAmount)) {
-//         incomes.push({ amount: incomeAmount });
-//     }
-
-//     console.log('Received POST /income: ', incomeAmount);
-
-//     // send JSON response
-//     // res.status(200).json({
-//     //     message: 'POST request received',
-//     //     receivedData: body
-//     // });
-
-//     // res.redirect('/summary');
-// });
-
-// export default router
