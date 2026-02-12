9bde2dcfd87eb54477b8b68375177e2b25f52ddc
EmmaRask <emmarask@yahoo.com>
2026-02-13 00:50:53 +0100

    added style in buttons in index.pug, add eventlistener in budgetPanel.js and styling in  css.

--- public/Js/budgetPanel.js
+++ public/Js/budgetPanel.js
@@ -9,3 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const incomeForm = document.querySelector(".income-form");
+
+  if (incomeForm) {
+    incomeForm.addEventListener("submit", () => {
+      const btn = incomeForm.querySelector("button");
+      btn.classList.add("saved");
+
+      setTimeout(() => {
+        btn.classList.remove("saved");
+      }, 600);
+    });
+  }
+});
